8bca3c1b494a35b6ac68a4ed653b56f06985e73c
audrey murua <audrey.murua1@gmail.com>
2022-04-16 23:29:30 -0500

    completed second challenge

--- practice-problems.js
+++ practice-problems.js
@@ -19,3 +19,23 @@ console.log(addition(3,2));
 console.log(addition(-3,-6));
 console.log(addition(7,3));
 
+
+// Coding Challenge #2
+
+/* Return the Next Number from the Integer Passed
+
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2 */
+
+function addIncrement (i) {
+    return ++i;
+}
+
+console.log(addIncrement(0));
+console.log(addIncrement(9));
+console.log(addIncrement(-3));
