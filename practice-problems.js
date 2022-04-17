ef3a58a71fd2f4018a8fe8da21dbd99cb7e021a7
audrey murua <audrey.murua1@gmail.com>
2022-04-16 23:49:26 -0500

    completed challenge

--- practice-problems.js
+++ practice-problems.js
@@ -60,3 +60,29 @@ function howManySeconds(hrs) {
 console.log(howManySeconds(2));
 console.log(howManySeconds(10));
 console.log(howManySeconds(24));
+
+// Coding Challenge #4
+
+/* Return the Remainder from Two Numbers
+
+There is a single operator in JavaScript, capable of providing the remainder of a division operation. 
+Two numbers are passed as parameters. 
+The first parameter divided by the second parameter will have a remainder, possibly zero.
+ Return that value.
+
+ remainder(1, 3) ➞ 1
+
+remainder(3, 4) ➞ 3
+
+remainder(-9, 45) ➞ -9
+
+remainder(5, 5) ➞ 0 */
+
+function remainder(a, b) {
+    return a % b;
+}
+
+console.log(remainder(1, 3));
+console.log(remainder(3, 4));
+console.log(remainder(-9, 45));
+console.log(remainder(5, 5));
