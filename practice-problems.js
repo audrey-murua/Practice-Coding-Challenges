fee67a428a17071d29300ace98d4bc8beffdeaf4
audrey murua <audrey.murua1@gmail.com>
2022-04-24 16:46:09 -0500

    completed challenge

--- practice-problems.js
+++ practice-problems.js
@@ -122,3 +122,27 @@ function calcPower(voltage, current) {
 console.log(calcPower(230, 10));
 console.log(calcPower(110, 3));
 console.log(calcPower(480, 20));
+
+// Coding Challenge #6
+
+/* Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+cubes(3) ➞ 27
+
+cubes(5) ➞ 125
+
+cubes(10) ➞ 1000
+
+function cubes(a) {
+	retunr a ** 3
+}*/
+
+function cubes(c) {
+    return Math.pow(c, 3);
+}
+
+console.log(cubes(3));
+
+console.log(cubes(5));
+
+console.log(cubes(10));
